ba47affe6b1184ba6bae251945bf2c9d5691ebd9
Chenlong Cai <ccai5632@uni.sydney.edu.au>
2022-10-16 14:09:15 +1100

    fix autocomplete

--- src/components/CreateEvent.vue
+++ src/components/CreateEvent.vue
@@ -93,14 +93,15 @@ export default {
             };
             const options = {
                 bounds: defaultBounds,
-                fields: ["address_components", "geometry", "icon", "name"],
+                fields: ["address_components", "geometry", "name","formatted_address"],
                 strictBounds: false,
                 types: ["establishment"],
             };
             const autocomplete = new google.maps.places.Autocomplete(this.$refs["address"], options);
             autocomplete.addListener("place_changed", () => {
                 const place = autocomplete.getPlace();
-                this.event.location = place.name;
+                console.log(place);
+                this.event.location = place.name + ", " + place.formatted_address;
             });
         });
         this.$refs.selectImage.addEventListener('change', async () => {
